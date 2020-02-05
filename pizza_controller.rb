d8e0f43ad12665c99b6ff1a5681cf3142380451f
cowannoel <cowannoel@gmail.com>
2020-02-05 16:30:54 +0000

    Initial commit

---
+++ pizza_controller.rb
@@ -0,0 +1,56 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative('./models/pizza_order')
+also_reload('./models/*')
+
+# READ - all/index
+get '/pizza-orders' do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+#CREATE - NEW
+
+get '/pizza-orders/new' do
+  erb(:new)
+end
+
+
+#CREATE - CREATE
+
+post '/pizza-orders' do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+end
+
+
+# READ - one/show
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+
+#UPDATE
+
+get '/pizza-orders/:id/edit' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:edit)
+end
+
+post '/pizza-orders/:id' do
+  order = PizzaOrder.new(params)
+  order.update()
+  redirect "/pizza-orders"
+end
+
+#DELETE
+
+post '/pizza-orders/:id/delete' do
+  order = PizzaOrder.find(params[:id])
+  order.delete
+  erb(:delete)
+end
